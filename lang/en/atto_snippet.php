881cde602e2712f80e26fddcff0b794befddba53
justinhunt <bitwalkerjapan@gmail.com>
2017-05-16 16:31:25 +0900

    removed concatenated strings from lang file to keep AMOS happy.

--- lang/en/atto_snippet.php
+++ lang/en/atto_snippet.php
@@ -49,7 +49,5 @@ $string['snippetpageheading'] = '{$a}';
 $string['generalsettings'] = 'General settings';
 $string['snippets'] = 'Snippets';
 $string['loadfromtheme'] ='Load snippets from theme';
-$string['loadfromtheme_desc'] ='Some Moodle themes ship with their own snippet presets. If this setting is set to "yes" then theme snippets will ' .
-    'automatically appear in the Atto snippet dialog. Theme snippet presets can be applied from the ' .
-    'dropdown box on the snippet settings page regardless of this setting ';
+$string['loadfromtheme_desc'] ='Some Moodle themes ship with their own snippet presets. If this setting is set to "yes" then theme snippets will automatically appear in the Atto snippet dialog. Theme snippet presets can be applied from the dropdown box on the snippet settings page regardless of this setting ';
 
